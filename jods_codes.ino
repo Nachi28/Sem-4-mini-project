303b2e410144a7bc7af86e27b8501adceb83fd51
Nachi28 <79222622+Nachi28@users.noreply.github.com>
2022-04-23 04:00:46 +0530

    second

--- jods_codes.ino
+++ jods_codes.ino
@@ -2,17 +2,17 @@
 #include <Servo.h>
 
 //Motor setup
-int sp_pinA = 9;  //sp_pinA connected to digital pin 3
-int sp_pinB = 11;  //sp_pinB connected to digital pin 9
-int MOTOR_A1 = 8; // MOTOR_A1 connected to digital pin 4
-int MOTOR_A2 = 10; // MOTOR_A2 connected to digital pin 5
-int MOTOR_B1 = 12; // MOTOR_B1 connected to digital pin 6
-int MOTOR_B2 = 13; // MOTOR_B2 connected to digital pin 7
+int sp_pinA = 9;  
+int sp_pinB = 11;  
+int MOTOR_A1 = 8; 
+int MOTOR_A2 = 10;
+int MOTOR_B1 = 12; 
+int MOTOR_B2 = 13; 
 Servo servo;
 
 //IR setup 
-int IR_RIGHT = A0; // RIGHT sensor connected to analog pin A0
-int IR_LEFT = A1;  // LEFT sensor connected to analog pin A1
+int IR_RIGHT = A0; 
+int IR_LEFT = A1; 
 int button = 12;
 
 int ini_x_cordi = 0;
@@ -20,30 +20,31 @@ int final_x_cordi = 2;
 
 
 //Ultrasonic setup
-#define TRIG A2 // TRIG PIN connected to analog pin A2
-#define ECHO A3 // ECHO PIN connected to analog pin A3
-#define MAX_DISTANCE 200 // Define Maximum Distance
+#define TRIG A2
+#define ECHO A3 
+#define MAX_DISTANCE 200
 int sp_valueA = 60;
 int sp_valueB = 60;
 int distance = 100;
+
+//NewPing sonar(trigger_pin, echo_pin [, max_cm_distance]);
 NewPing sonar(TRIG, ECHO, MAX_DISTANCE); 
 
 void setup() 
 {
   
   Serial.begin(9600);  
-  servo.attach(5);
-  pinMode(sp_pinA, OUTPUT); // initialize sp_pinA pin as an output
-  pinMode(sp_pinB, OUTPUT); // initialize sp_pinB pin as an output
-  pinMode(MOTOR_A1, OUTPUT); // initialize MOTOR_A1 pin as an output
-  pinMode(MOTOR_A2, OUTPUT); // initialize MOTOR_A2 pin as an output
-  pinMode(MOTOR_B1, OUTPUT); // initialize MOTOR_B1 pin as an output
-  pinMode(MOTOR_B2, OUTPUT); // initialize MOTOR_B2 pin as an output
-  pinMode(IR_RIGHT, INPUT); // initialize RIGHT pin as an input
-  pinMode(IR_LEFT, INPUT);  // initialize LEFT pin as an input
+  pinMode(sp_pinA, OUTPUT); 
+  pinMode(sp_pinB, OUTPUT); 
+  pinMode(MOTOR_A1, OUTPUT); 
+  pinMode(MOTOR_A2, OUTPUT); 
+  pinMode(MOTOR_B1, OUTPUT);
+  pinMode(MOTOR_B2, OUTPUT); 
+  pinMode(IR_RIGHT, INPUT);
+  pinMode(IR_LEFT, INPUT);
+  
+  //pullup is for button ie.continously checks values ig
   pinMode(button, INPUT_PULLUP);
-  servo.write(90);
-
   Serial.println("Initial X cordi =");
   Serial.println(ini_x_cordi);
   Serial.println("Final X cordi =");
@@ -52,14 +53,14 @@ void setup()
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
 //  digitalWrite(button, LOW);
   if(digitalRead(button)){
-//    Serial.println("high");
+//  Serial.println("high");
     sp_valueA = 80;
     sp_valueB = 80;
    
-    //Stop function to reach final destination
+    //Acts like Stop function and counts crosses to reach final destination
     if (!digitalRead(IR_RIGHT) && !digitalRead(IR_LEFT)) 
     {
       Serial.println("X cordi =");
@@ -125,7 +126,7 @@ void loop() {
     delay(1000);
     
     turnLeft();
-    delay(3000);//check time practically
+    delay(3000);//check delay time practically
     Stop();
     Serial.println("Reached and avoided the obstacle");
     
@@ -216,6 +217,7 @@ void go(void)
 }
 
 
-//remembaer void loop check one value then move on then starts a new loop...so previous values are gone
-//can improve forward function for stepwise with while loop (tooez)
+//remember void loop checks one value then move on then starts a new loop...so previous values are gone
+//can improve forward function for stepwise with while loop just as in backward function (tooez)
 //calibrate motor speeds and all delay times
+//if possible take square grid
